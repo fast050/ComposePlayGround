18ab94c1c5c4e19be370bd05f5cefb0f6c2688d6
Khalid <khalidwork050@outlook.com>
2023-04-25 10:47:07 +0400

    add dependency and init state

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.composeplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.magnifier
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+          PreviewGreeting()
+       }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Column {
+            Text (text = "Hello $name!")
+            Spacer(modifier = Modifier.size(10.dp))
+            Text (text = "[bruh] $name!")
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewGreeting() {
+        Greeting("Android Good")
     }
 }
